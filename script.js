afd8c99afd3807ac9b2da49101c883ca71f83c73
ANU-2524 <anusoni25.2006@gmail.com>
2025-04-20 23:58:10 +0530

    Completed

--- script.js
+++ script.js
@@ -14,10 +14,16 @@ document.addEventListener('DOMContentLoaded', function() {
       name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
   }
 
-  // 1. Get the value of the 'count' cookie
-  // 2. If the cookie exists, increment the value and update the cookie
-  // 3. If the cookie does not exist, create it and set the value to 1
-  // 4. Display the count on the webpage
+  let count = parseInt(getCookie("count")) ;
+  if (count !== null){
+    count += 1;
+  }else{
+    count = 1 ;
+  }
 
-  // your code here
+  setCookie("count" , count , 25) ;
+  let display = document.createElement("h3") ;
+  display.innerText = `Counts Cookie : ${count}` ;
+  document.body.appendChild(display) ; 
+ 
 });
